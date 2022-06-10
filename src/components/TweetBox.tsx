683e4a1052a2a943bbcc8cf427e7496be1317b48
Rayan Kazi <rayankazi7515@gmail.com>
2022-06-10 12:34:41 -0700

    feat: built tweet box

---
+++ src/components/TweetBox.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @next/next/no-img-element */
+import { NextComponentType } from "next";
+import {
+  CalendarIcon,
+  EmojiHappyIcon,
+  LocationMarkerIcon,
+  PhotographIcon,
+  SearchCircleIcon,
+} from "@heroicons/react/outline";
+import { useState } from "react";
+
+const TweetBox: NextComponentType = () => {
+  const [inputText, setInputText] = useState<string>("");
+
+  return (
+    <div className="flex space-x-2 p-5">
+      <img
+        className="mt-4 h-14 w-14 rounded-full object-cover"
+        src="/images/default-avatar.jpg"
+        alt="Default Avatar"
+      />
+
+      <div className="flex flex-1 items-center pl-2">
+        <form className="flex flex-1 flex-col">
+          <input
+            value={inputText}
+            onChange={e => setInputText(e.target.value)}
+            type="text"
+            placeholder="What's Happening?"
+            className="h-24 w-full text-xl outline-none placeholder:text-xl"
+          />
+
+          <div className="flex items-center">
+            <div className="flex flex-1 space-x-2 text-twitter">
+              <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <SearchCircleIcon className="h-5 w-5" />
+              <EmojiHappyIcon className="h-5 w-5" />
+              <CalendarIcon className="h-5 w-5" />
+              <LocationMarkerIcon className="h-5 w-5" />
+            </div>
+
+            <button
+              disabled={!inputText}
+              className="rounded-full bg-twitter px-5 py-2 font-bold text-white disabled:opacity-40"
+            >
+              Tweet
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default TweetBox;
